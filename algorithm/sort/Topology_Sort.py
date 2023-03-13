80af49613812a6dbf9624dda7f7f5bb3e0265471
Min Jeon <mulsoap0504@gmail.com>
2023-03-13 22:55:21 +0900

    Update Topology_Sort.py

--- algorithm/sort/Topology_Sort.py
+++ algorithm/sort/Topology_Sort.py
@@ -7,6 +7,8 @@
 # 3) 새롭게 진입 차수가 0이 된 노드를 큐에 넣는다.
 # 4) 큐가 빌 때까지 (2), (3)을 반복한다.
 
+# 시간복잡도: O(v+e)
+
 from collections import deque
 
 # 노드의 개수와 간선 개수 입력받기
@@ -31,7 +33,7 @@ def topology_sort():
     q = deque()
 
     # 진입 차수가 0인 노드들이 큐에 담김
-    for i in range(1,v+1):
+    for i in range(1,v+1):  # O(v)
         if indegree[i] == 0:
             q.append(i)
 
