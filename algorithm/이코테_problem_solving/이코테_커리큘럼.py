cff756ea5e38919a367f74ae771ca4bdde0e562a
Min Jeon <mulsoap0504@gmail.com>
2023-03-16 13:44:14 +0900

    update

---
+++ algorithm/이코테_problem_solving/이코테_커리큘럼.py
@@ -0,0 +1,14 @@
+# 커리 큘럼
+# 난이도 2
+# 시간제한 2초
+# 메모리 제한 256MB
+
+# 강의 n개
+n = int(input())
+# 진입 차수
+indegree = [0]*(n+1)
+# 그래프
+graph = [[] for _ in range(n)]
+
+for i in range(n):
+    graph[i].append()
