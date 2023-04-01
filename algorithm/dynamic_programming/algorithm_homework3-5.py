9cfd16e8a8984a5e8283654eeca1011b1174a792
ricecake_ssamanko <mulsoap0504@gmail.com>
2023-04-01 17:07:39 +0900

    Update algorithm_homework3-5.py

--- algorithm/dynamic_programming/algorithm_homework3-5.py
+++ algorithm/dynamic_programming/algorithm_homework3-5.py
@@ -1,59 +1,57 @@
 n = int(input())
-d = []  #ai = di*di+1, d0 = 0
-d.append(0) 
-for x in list(map(int, input().split())):
-    d.append(x)
 
-opt = [['x']*(n+1) for _ in range(n+1)]
-p = [['x']*(n+1) for _ in range(n+1)]
-for i in range(1,n+1):
-    p[i][i] = 0
+p = []
+p.append(0.)
+
+for x in list(map(float, input().split())):
+    p.append(x)
+
+opt = [[0] * (n + 1) for _ in range(n + 2)]  # opt 배열
+array_k = [[0] * (n + 1) for _ in range(n + 1)]  # 최소 k를 담는 배열
+
+def sum_probability(i, j):
+    result = 0
+    for idx in range(i, j+1):
+        result += p[idx]
+    return result
+
 
 def cal_opt(n):
-    for i in range(n + 1):
-        opt[i][i] = 0
-    for diagonal in range(1,n+1):
-        for i in range(1,n-diagonal+1):
+    for i in range(1, n + 1):
+        opt[i][i] = 1.0
+        array_k[i][i] = i
+
+    for diagonal in range(1, n + 1):
+        for i in range(1, n - diagonal + 1):
             result = int(1e9)
             result_k = -1
-            j = i+diagonal
-            for k in range(i,j):
-                if result > opt[i][k]+opt[k+1][j]+d[i]*d[k+1]*d[j+1]:
-                    result = opt[i][k]+opt[k+1][j]+d[i]*d[k+1]*d[j+1]
+            j = i + diagonal
+
+            for k in range(i, j+1):
+                tmp = 1 + (sum_probability(i, k - 1) / sum_probability(i, j)) * opt[i][k - 1] + (sum_probability(k + 1, j) / sum_probability(i, j)) * opt[k + 1][j]
+                if result > tmp:
+                    result = tmp
                     result_k = k
             opt[i][j] = result
-            p[i][j] = result_k
+            array_k[i][j] = result_k
+
 
 cal_opt(n)
-for i in range(1,n+1):
-    for x in opt[i]:
-        if x!=-1:
-            print(x,end=" ")
+
+for i in range(1, n + 1):
+    for j in range(1,n+1):
+        x = opt[i][j]
+        if x!=0:
+            print(round(x,2), end=" ")
         else:
-            print(end=" ")
+            print(end="    ")
     print()
 print()
 
-for i in range(1,n+1):
-    print(f"{i}:",end=" ")
-    for x in p[i]:
-        if x!=-1:
-            print(x,end=" ")
+for i in range(1, n + 1):
+    for x in array_k[i]:
+        if x != 0:
+            print(x, end=" ")
         else:
-            print(end=" ")
+            print(end="  ")
     print()
-
-def order (i,j):
-    if i == j:
-        print('A',i,end=" ")
-    else:
-        k = p[i][j]
-        print("(", end=" ")
-        order(i, k)
-        order(k + 1, j)
-        print(")",end=" ")
-
-order(1,n)
-
-
-
