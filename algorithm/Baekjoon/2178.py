7b8f563b665e75a573d0d1281310daf702eb94d4
Min Jeon <mulsoap0504@gmail.com>
2023-06-17 16:35:58 +0900

    Create 2178.py

---
+++ algorithm/Baekjoon/2178.py
@@ -0,0 +1,30 @@
+from collections import deque
+import sys
+
+n, m = map(int, input().split())
+graph = [list(map(int,input())) for _ in range(n)]
+
+dx = [0,1,0,-1]
+dy = [-1,0,1,0]
+
+def bfs():
+    queue = deque([(0,0)])
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            elif graph[nx][ny] == 0:
+                continue
+            elif graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+    return graph[n-1][m-1]
+
+print(bfs())
+
