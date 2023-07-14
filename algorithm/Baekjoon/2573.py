aca64ffa789289bd5015df323efc9a2498bc4536
Min Jeon <mulsoap0504@gmail.com>
2023-07-14 09:08:28 +0900

    Update 2573.py

--- algorithm/Baekjoon/2573.py
+++ algorithm/Baekjoon/2573.py
@@ -1,87 +1,119 @@
+# import sys
+# from collections import deque
+#
+# input = sys.stdin.readline
+#
+# graph = []
+# n, m = list(map(int, input().split()))
+#
+# for _ in range(n):
+#     graph.append(list(map(int, input().split())))
+#
+# dx = [-1, 0, 1, 0]
+# dy = [0, 1, 0, -1]
+#
+#
+# def bfs(x, y):
+#     visited[x][y] = True
+#     queue = deque([(x, y)])
+#
+#     while queue:
+#         x, y = queue.popleft()
+#         sea = 0
+#
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#             if nx < 0 or nx > n or ny < 0 or nx > m:
+#                 continue
+#             if graph[nx][ny] == 0:
+#                 sea += 1
+#             if not visited[nx][ny] and graph[nx][ny] != 0:
+#                 queue.append([nx, ny])
+#                 visited[nx][ny] = True
+#
+#         adjacent_sea_num[x][y] = sea
+#
+#
+# result = 0
+# for k in range(10):
+#     visited = [[False] * m for _ in range(n)]
+#     adjacent_sea_num = [[0] * m for _ in range(n)]
+#     partition = 0
+#
+#     for i in range(n):
+#         for j in range(m):
+#             if not visited[i][j] and graph[i][j] != 0:
+#                 bfs(i, j)
+#                 partition += 1
+#
+#     if partition >= 2:
+#         result = k
+#         break
+#
+#     for i in range(n):
+#         for j in range(m):
+#             graph[i][j] = max(0,graph[i][j]-adjacent_sea_num[i][j])
+#
+# print(result)
+
 import sys
 from collections import deque
-
-def show_array(graph):
-    for line in graph:
-        print(line)
-    print()
-
 input = sys.stdin.readline
 
-graph = []
-n, m = list(map(int, input().split()))
 
-max_height = 0
-for _ in range(n):
-    heights = list(map(int, input().split()))
-    for height in heights:
-        max_height = max(max_height, height)
-    graph.append(heights)
+def bfs(x, y):
+    q = deque([(x, y)])
+    visited[x][y] = 1
+    seaList = []
 
-print(max_height)
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
-
-def bfs(x, y, visited, adjacent_sea_water):
-    if graph[x][y] == 0:
-        visited[x][y] = True
-        return False
-
-    queue = deque()
-    queue.append([x,y])
-
-    while queue:
-        x, y = queue.popleft()
-        visited[x][y] = True
-
+    while q:
+        x, y = q.popleft()
+        sea = 0
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if not graph[nx][ny]:
+                    sea += 1
+                elif graph[nx][ny] and not visited[nx][ny]:
+                    q.append((nx, ny))
+                    visited[nx][ny] = 1
+        if sea > 0:
+            seaList.append((x, y, sea))
+    for x, y, sea in seaList:
+        graph[x][y] = max(0, graph[x][y] - sea)
 
-            if 0 >= nx or nx >= n-1 or 0 >= ny or ny >= m-1:
-                continue
-            else:
-                if not visited[nx][ny] and graph[nx][ny] != 0:
-                    queue.append([nx,ny])
-                    visited[nx][ny] = True
-    return True
-
-result = 0
-for year in range(max_height):
-    show_array(graph)
-    visited = [[False] * m for _ in range(n)]
-    adjacent_sea_water = [[0] * m for _ in range(n)]
-
-    partition = 0  # 빙산 덩어리 개수
-    for i in range(n):
-        for j in range(m):
-            if bfs(i, j, visited, adjacent_sea_water):
-                partition += 1
-    
-
-    for i in range(1,n-1):
-        for j in range(1,m-1):
-            if graph[i][j] != 0:
-                for k in range(4):
-                    nx = i+dx[k]
-                    ny = j+dy[k]
-                    if graph[nx][ny] == 0:
-                        adjacent_sea_water[i][j]+=1
-
-    print(f"year:{year}")
-    print("<graph>")
-    show_array(graph)
-    print("<adjacent_sea_water>")
-    show_array(adjacent_sea_water)
-    print()
+    return 1
 
 
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] - adjacent_sea_water[i][j] >= 0:
-                graph[i][j] -= adjacent_sea_water[i][j]
-            else:
-                graph[i][j] = 0
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-print(result)
+ice = []
+for i in range(n):
+    for j in range(m):
+        if graph[i][j]:
+            ice.append((i, j))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+year = 0
+
+while ice:
+    visited = [[0] * m for _ in range(n)]
+    delList = []
+    group = 0
+    for i, j in ice:
+        if graph[i][j] and not visited[i][j]:
+            group += bfs(i, j)
+        if graph[i][j] == 0:
+            delList.append((i, j))
+    if group > 1:
+        print(year)
+        break
+    ice = sorted(list(set(ice) - set(delList)))
+    year += 1
+
+if group < 2:
+    print(0)
