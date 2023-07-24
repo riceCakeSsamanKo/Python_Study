6424eac6dd39d7e9c4905453c47374a6098c0504
ricecake_ssamanko <mulsoap0504@gmail.com>
2023-07-24 22:52:40 +0900

    Update 1012.py

--- algorithm/Baekjoon/1012.py
+++ algorithm/Baekjoon/1012.py
@@ -3,28 +3,46 @@ from collections import deque
 
 input = sys.stdin.readline
 
-t = int(input())
-
 dx = [-1, 0, 1, 0]
 dy = [0, -1, 0, 1]
 
-
 def bfs(graph, x, y):
-    if visited[x][y]:
+    if graph[x][y] == 0 or visited[x][y]:
         return 0
-    queue = deque([(x,y)])
 
+    queue = deque([(x, y)])
+    visited[x][y] = True
     while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                continue
+            if graph[nx][ny] == 1 and not visited[nx][ny]:
+                visited[nx][ny] = True
+                queue.append((nx,ny))
+
+    return 1
 
 
-
+# 테스트 케이스 개수
+t = int(input())
 
 for _ in range(t):
     m, n, k = map(int, input().split())
+    result = 0
+    graph = [[0] * n for _ in range(m)]
+    visited = [[False] * n for _ in range(m)]
 
-    graph = [[0] * m for _ in range(n)]
-    visited = [[False] * m for _ in range(n)]
-
-    for i in range(k):
-        x, y = map(int, input())
+    for _ in range(k):
+        x, y = map(int, input().split())
         graph[x][y] = 1
+
+    for i in range(m):
+        for j in range(n):
+            result += bfs(graph,i,j)
+
+    print(result)
