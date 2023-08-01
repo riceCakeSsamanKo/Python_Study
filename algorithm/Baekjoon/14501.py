903805336c719853c050517e6c4d7921e41d3fc3
Min Jeon <mulsoap0504@gmail.com>
2023-08-01 17:10:06 +0900

    Update 14501.py

--- algorithm/Baekjoon/14501.py
+++ algorithm/Baekjoon/14501.py
@@ -6,27 +6,30 @@ n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
 max_value = 0
-
-
 def recur(idx, total):
     global max_value
+
+    if idx >= n:
+        return
 
     day = graph[idx][0]
     profit = graph[idx][1]
 
-    if idx+day > n:
+    if idx+1 == n:
+        max_value = max(max_value, total)
+    else:
+        # 상담하지 않는 경우
+        recur(idx + 1, total)
+
+    if idx + day > n:
         max_value = max(max_value, total)
         return
-    elif idx+day == n:
-        max_value = max(max_value,total+profit)
+    elif idx + day == n:
+        max_value = max(max_value, total + profit)
         return
-
-    # 상담하는 경우
-    recur(idx + day, total + profit)
-    # 상담하지 않는 경우
-    recur(idx + 1, total)
-
+    else:
+        # 상담하는 경우
+        recur(idx + day, total + profit)
 
 recur(0, 0)
-
 print(max_value)
