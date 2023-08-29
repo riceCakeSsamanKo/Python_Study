c24fc745be68b540b5743631df1f98af9d950f8c
Min Jeon <mulsoap0504@gmail.com>
2023-08-29 22:48:40 +0900

    Update 5427.py

--- algorithm/Baekjoon/5427.py
+++ algorithm/Baekjoon/5427.py
@@ -4,8 +4,7 @@ from collections import deque
 input = sys.stdin.readline
 
 dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
-
+dy = [0, 1, 0, -1]
 
 def show_array(graph):
     for line in graph:
@@ -14,37 +13,42 @@ def show_array(graph):
 
 
 def bfs():
-    cnt = 0
+    cnt = 1
 
     while q:
-        cnt += 1
-        x, y = q.popleft()
-        # 1) cur == "fire": (x,y)에 불
-        # 2) cur == "True": (x,y)에 사람
-        # 3) cur == "false": (x,y)는 벽이거나 길
-        cur = visited[x][y]
 
-        show_array(visited)
+        x, y, cond = q.popleft()
+        # 1) cond == "fire": (x,y)에 불
+        # 2) cond == "True": (x,y)에 사람
+        # 3) cond == "false": (x,y)는 벽이거나 길
 
+        isMove = False
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
             if 0 <= nx < h and 0 <= ny < w:
-                if cur == "fire":
+                if cond == "fire":
                     if visited[nx][ny] != "fire" and graph[nx][ny] != '#':
                         # 불꽃 이동
                         visited[nx][ny] = "fire"
 
-                if cur == True:
-                    if visited[nx][ny] != "fire" and graph[nx][ny] == '.':
+                elif cond == "path":
+                    if visited[nx][ny]==False and graph[nx][ny] == '.':
                         # 사람 이동
-                        visited[nx][ny] = True
+                        visited[nx][ny] = "path"
+                        isMove = True
 
-                q.append([nx, ny])
-            elif cur != "fire":
-                return cnt
+                q.append([nx, ny, cond])
+            else:
+                if cond != "fire":
+                    return cnt
+        if isMove:
+            show_array(visited)
+            cnt += 1
     return "impossible"
+
+
 
 
 t = int(input())
@@ -60,14 +64,14 @@ for _ in range(t):
         graph.append(list(input().strip()))
         for j in range(w):
             if graph[i][j] == '@':
-                visited[i][j] = True
+                visited[i][j] = "path"
                 start_x, start_y = i, j
             elif graph[i][j] == '*':
                 visited[i][j] = "fire"
                 # 불꽃의 초기 위치를 먼저 저장
-                q.append([i, j])
+                q.append([i, j, "fire"])
 
-    # 시작 초기 위치를 나중 저장
-    q.append([start_x, start_y])
+    # 시작 초기 위치를 나중
+    q.append([start_x, start_y, "path"])
     result = bfs()
     print(result)
