6b49e5f43cc3e32c3ebef600b5440b7a9c192a1a
Min Jeon <mulsoap0504@gmail.com>
2023-09-02 17:48:41 +0900

    Update 1937.py

--- algorithm/Baekjoon/1937.py
+++ algorithm/Baekjoon/1937.py
@@ -1,57 +1,59 @@
 import sys
 
+# input = sys.stdin.readline
+# sys.setrecursionlimit(10 ** 6)
+#
+#
+# def show(graph):
+#     for line in graph:
+#         print(line)
+#     print()
+#
+#
+# # dp와 dfs 사용
+# # 이차원 dp 테이블 만들고 각 그래프 칸을 dfs로 탐색해서 최대 값을 dp에 저장
+#
+# dx = [-1, 0, 1, 0]
+# dy = [0, 1, 0, -1]
+#
+#
+# def dfs(x, y, cnt):
+#     global max_value
+#     max_value = max(cnt, max_value)
+#
+#     for k in range(4):
+#         nx = x + dx[k]
+#         ny = y + dy[k]
+#
+#         # 종료 조건
+#         if nx < 0 or nx >= n or ny < 0 or ny >= n:
+#             continue
+#
+#         # 탐색 조건
+#         if graph[nx][ny] > graph[x][y]:
+#             if dp[nx][ny] == 0:
+#                 dfs(nx, ny, cnt + 1)
+#             else:
+#                 max_value = max(max_value, dp[nx][ny] + cnt + 1)
+#
+#
+# n = int(input())
+#
+# dp = [[0] * n for _ in range(n)]
+# graph = []
+# for i in range(n):
+#     graph.append(list(map(int, input().split())))
+#
+# result = 0
+# for i in range(n):
+#     for j in range(n):
+#         max_value = 0
+#         dfs(i, j, 0)
+#         dp[i][j] = max_value
+#         result = max(result, max_value)
+#
+# show(graph)
+# show(dp)
+# print(result + 1)
+
 input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 6)
-
-
-def show(graph):
-    for line in graph:
-        print(line)
-    print()
-
-
-# dp와 dfs 사용
-# 이차원 dp 테이블 만들고 각 그래프 칸을 dfs로 탐색해서 최대 값을 dp에 저장
-
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
-
-def dfs(x, y, cnt):
-    global max_value
-    max_value = max(cnt, max_value)
-
-    for k in range(4):
-        nx = x + dx[k]
-        ny = y + dy[k]
-
-        # 종료 조건
-        if nx < 0 or nx >= n or ny < 0 or ny >= n:
-            continue
-
-        # 탐색 조건
-        if graph[nx][ny] > graph[x][y]:
-            if dp[nx][ny] == 0:
-                dfs(nx, ny, cnt + 1)
-            else:
-                max_value = max(max_value, dp[nx][ny] + cnt + 1)
-
-
-n = int(input())
-
-dp = [[0] * n for _ in range(n)]
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input().split())))
-
-result = 0
-for i in range(n):
-    for j in range(n):
-        max_value = 0
-        dfs(i, j, 0)
-        dp[i][j] = max_value
-        result = max(result, max_value)
-
-show(graph)
-show(dp)
-print(result + 1)
