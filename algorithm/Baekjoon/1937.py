1f1dc0fb7a4b2d2911580d7ddf8c4c5134247ed4
Min Jeon <mulsoap0504@gmail.com>
2023-09-05 14:25:03 +0900

    Update 1937.py

--- algorithm/Baekjoon/1937.py
+++ algorithm/Baekjoon/1937.py
@@ -3,55 +3,44 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 6)
 
-
-def show(graph):
-    for line in graph:
-        print(line)
-    print()
-
+# def show(graph):
+#     for line in graph:
+#         print(line)
+#     print()
 
 # dp와 dfs 사용
 # 이차원 dp 테이블 만들고 각 그래프 칸을 dfs로 탐색해서 최대 값을 dp에 저장
-
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
 
-def dfs(x, y, cnt):
-    global max_value
-    max_value = max(cnt, max_value)
+def dfs(x, y):
+    if dp[x][y] != 0:
+        return dp[x][y]
+    dp[x][y] = 1
+
     for k in range(4):
         nx = x + dx[k]
         ny = y + dy[k]
 
-        # 종료 조건
-        if nx < 0 or nx >= n or ny < 0 or ny >= n:
-            continue
-
         # 탐색 조건
-        if graph[nx][ny] > graph[x][y]:
-            if dp[nx][ny] == -1:
-                dfs(nx, ny, cnt + 1)
-            else:
-                max_value = max(max_value, dp[nx][ny] + cnt + 1)
+        if 0 <= nx < n and 0 <= ny < n and graph[x][y] < graph[nx][ny]:
+            dp[x][y] = max(dp[x][y], dfs(nx, ny) + 1)
+    return dp[x][y]
 
 
 n = int(input())
 
-dp = [[-1] * n for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
 graph = []
 for i in range(n):
     graph.append(list(map(int, input().split())))
 
-result = 0
-max_value = 0
+answer = 0
 for i in range(n):
     for j in range(n):
-        max_value = 0
-        dfs(i, j, 0)
-        dp[i][j] = max_value
-    result = max(result, max_value)
+        result = dfs(i, j)
+        answer = max(result, answer)
 
-show(graph)
-show(dp)
-print(result + 1)
+# show(dp)
+print(answer)
