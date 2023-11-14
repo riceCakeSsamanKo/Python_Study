a305533979953bc607dfadd84f38bc9e471e1c76
Min Jeon <mulsoap0504@gmail.com>
2023-11-14 22:29:37 +0900

    Update main.py

--- deep_learning/cnn_practice1/main.py
+++ deep_learning/cnn_practice1/main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
-from torch.autograd import variable
+from torch.autograd import Variable
 import torch.nn.functional as F
 
 import torchvision
@@ -22,19 +22,18 @@ test_dataset = torchvision.datasets.FashionMNIST("/Users/jeonmin/Documents/study
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=100)  # train_dataset에서 100개 단위로 데이터를 묶어서 불러온다.
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=100)
 
-labels_map = {0: 'T-shirt', 1: "Trouser", 2: "Pullover",
-              3: "Dress", 4: "Coat", 5: "Sandal",
-              6: "Shirt", 7: "Sneaker", 8: "Bag", 9: "Ankle Boot"}  # 이미지 레이블 클래스
+labels_map = {0: 'T-Shirt', 1: 'Trouser', 2: 'Pullover', 3: 'Dress', 4: 'Coat', 5: 'Sandal', 6: 'Shirt',
+              7: 'Sneaker', 8: 'Bag', 9: 'Ankle Boot'}
 
-fig = plt.figure(figsize=(8, 8))  # 출력할 이미지의 가로 세로 길이. (단위 inch)
+fig = plt.figure(figsize=(8, 8))
 columns = 4
 rows = 5
 for i in range(1, columns * rows + 1):
-    img_xy = np.random.randint(len(train_dataset))  # train_dataset의 길이에서 임의의 랜덤한 숫자 생성
+    img_xy = np.random.randint(len(train_dataset));
     img = train_dataset[img_xy][0][0, :, :]
     fig.add_subplot(rows, columns, i)
     plt.title(labels_map[train_dataset[img_xy][1]])
-    plt.axis("off")
+    plt.axis('off')
     plt.imshow(img, cmap='gray')
 plt.show()
 
@@ -50,12 +49,62 @@ class FashionDNN(nn.Module):
     # 순전파 메서드
     def forward(self, input_data):
         out = input_data.view(-1, 784)  # 넘파이의 reshape. input_data의 크기를 (?, 784)로 변경하라. -1은 파이토치에게 알아서 차원을 맞추라는 의미
-        out = F.relu(self.fc1(out)) # 은닉층 1
+        out = F.relu(self.fc1(out))  # 은닉층 1
         out = self.drop(out)  # 과 최적화 방지를 위한 drop out(25%)
         out = F.relu(self.fc2(out))  # 은닉층 2
         out = F.relu(self.fc3(out))  # 은닉층 3
         return out
 
 
-dnn = FashionDNN()
-dnn.forward(train_da)
+learning_rate = 0.001
+model = FashionDNN()
+model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+print(model)
+
+'''cnn을 이용한 모델 학습'''
+num_epochs = 5
+count = 0
+loss_list = []
+iteration_list = []
+accuracy_list = []
+
+predictions_list = []
+labels_list = []
+
+for epoch in range(num_epochs):
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        train = Variable(images.view(100, 1, 28, 28))
+        labels = Variable(labels)
+
+        outputs = model(train)
+        loss = criterion(outputs, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        count += 1
+
+        if not (count % 50):
+            total = 0
+            correct = 0
+            for images, labels in test_loader:
+                images, labels = images.to(device), labels.to(device)
+                labels_list.append(labels)
+                test = torch.autograd.Variable(images.view(100, 1, 28, 28))
+                outputs = model(test)
+                predictions = torch.max(outputs, 1)[1].to(device)
+                predictions_list.append(predictions)
+                correct += (predictions == labels).sum()
+                total += len(labels)
+
+            accuracy = correct * 100 / total
+            loss_list.append(loss.data)
+            iteration_list.append(count)
+            accuracy_list.append(accuracy)
+
+        if not (count % 500):
+            print("Iteration: {}, Loss: {}, Accuracy: {}%".format(count, loss.data, accuracy))
